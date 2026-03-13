2ba781ea7470df59a0becc3dd06dff2d5dcf7d4a
BernardoAbaurre <102594574+BernardoAbaurre@users.noreply.github.com>
2026-03-13 16:56:38 -0300

    Fix connection strings

--- rei-do-chopp-site/src/environments/environment.prod.ts
+++ rei-do-chopp-site/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apis: {
-    reiDoChoppApi: "http://64.23.161.175:5000/api/",
+    reiDoChoppApi: "http://104.236.2.50:5000/api/",
     printApi: "https://host.docker.internal:8080/api/"
   },
 };
